bfac9aa6786f31417d83e3e2a2760225468a2059
Jhonas Palad <jhonasemmanuel@gmail.com>
2023-02-01 20:52:02 +0800

    new commit

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, Platform, StatusBar, Dimensions} from 'react-native';
 import RootNavigator from './src/navigation/RootNavigator';
-import { setStatusBarStyle} from 'expo-status-bar';
-
-const statusBarHeight = StatusBar.currentHeight;
+import {Box, NativeBaseProvider} from 'native-base';
 
 export default function App() {
-  React.useEffect(()=>{setStatusBarStyle("light")}, []);
+
   return (
-    <>
-      <SafeAreaView style={{
-        flex:0, 
-        paddingTop: Platform.OS === 'android' ? statusBarHeight: 0 ,
-        backgroundColor: 'tomato' }} 
-      />
-      <SafeAreaView 
-        style={{flex:1, position:'relative'}}>
+    <NativeBaseProvider>
+      <Box flex={1} safeAreaX _dark={{bg: '#334155'}} _light={{bg:'info.200'}}>
           <RootNavigator />
-      </SafeAreaView>
-    </>
+      </Box>
+    </NativeBaseProvider>
       
   );
 }
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,36 +1,41 @@
-import { Animated, Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Animated, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react'
+import React from 'react';
 import { FontAwesome5 } from '@expo/vector-icons';
-import textStyles from '../styles/TextStyles';
+import {Text, Box, Heading, Button, HStack} from 'native-base';
 
 const HomeHeader = props => {
-    const [count, setCount] = React.useState(0);
     const navigation = useNavigation();
+
     return (
-        <Animated.View style={{
-            backgroundColor:'tomato',
-            alignItems: 'stretch',
-            justifyContent: 'center',
-            padding: 12,
-            zIndex: 5
-        }}>
-            <View style={[{flexDirection:'row', justifyContent:'space-between', marginBottom: 10, paddingHorizontal:10}]}>
-                <View style={{flexDirection:'row',alignItems: 'center'}}>
-                    <FontAwesome5 name={'bars'} color="#f8fafc" size={20} />
-                    <Text style={[textStyles.textHeadline,{marginLeft: 10, color: "#f8fafc"}]}>All services</Text>
-                </View>
+        <Box alignItems='stretch'
+        justifyContent='center'
+        zIndex={5} safeAreaTop _light={{bg: 'info.200'}}>
+            <HStack justifyContent="space-between" marginBottom={2}>
+                <HStack padding={0} space={0} alignItems="center">
+                    <Button
+                        color="primary.100"
+                        size="sm"
+                        variant="ghost"
+                    >
+                        <FontAwesome5 name={'bars'} color="#0ea5e9" size={20} />
+                    </Button>
+                    <Heading letterSpacing={-1} size="md" color="info.500">All services</Heading>
+                </HStack>
                 <View>
                     <FontAwesome5 name={"user-alt"} color="#f8fafc" size={24} />
                 </View>
-            </View>
-            
-            <TouchableOpacity onPress={()=> navigation.navigate('Search')} style={[styles.input]}>
-                    <Text style={{color:"#6b7280"}}>
-                        Search
-                    </Text>
+            </HStack>
+            <Animated.View>
+                <TouchableOpacity onPress={()=> navigation.navigate('Search')} style={[styles.input]}>
+                        <Text style={{color:"#6b7280"}}>
+                            Search
+                        </Text>
                 </TouchableOpacity>
-        </Animated.View>
+
+            </Animated.View>
+        </Box>
+        
     )
 }
 
